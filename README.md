d19317d8d31055445945972af66b14c8ea294713
wusiquan <1781554387@qq.com>
2020-06-16 10:44:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# conventional-changelog-customize
+
+conventional-changelog v1.1.0
---
+++ git-raw-commit.js
@@ -0,0 +1,3 @@
+module.exports = {
+  format: '%B%n-hash-%n%H%n-gitTags-%n%d%n-committerDate-%n%ci%n-authorName-%n%an%n'
+}
