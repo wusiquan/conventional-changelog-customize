3203075269cb99beda31dc1cf91f6346c7c25b4a
wusiquan <1781554387@qq.com>
2020-06-18 15:43:43 +0800

    增加standard-version配置文件

--- parser-opts.js
+++ parser-opts.js
@@ -7,6 +7,7 @@ module.exports = {
     'scope',
     'subject'
   ],
+  'issue-prefixes-case-sensitive': false,
   noteKeywords: ['BREAKING CHANGE'],
   revertPattern: /^(?:Revert|revert:)\s"?([\s\S]+?)"?\s*This reverts commit (\w*)\./i,
   revertCorrespondence: ['header', 'hash']
